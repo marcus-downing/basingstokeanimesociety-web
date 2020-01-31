c9afa555abdcf66868ba81c9b6b9a37d8f67e094
Marcus Downing <marcus@bang-on.net>
2020-01-31 14:58:19 +0000

    A make process

---
+++ dist/script.js
@@ -0,0 +1,17 @@
+
+// select a background image
+function selectBackground() {
+    var LIMIT = 3;
+
+    var hour = new Date().getHours();
+    var isDay = hour >= 6 && hour < 18;
+    var bgNum = 1+Math.floor(Math.random() * LIMIT);
+    var bodyClass = (isDay ? "day-" : "night-")+bgNum;
+
+    document.getElementsByTagName("BODY")[0].className = bodyClass;
+}
+
+selectBackground();
+setInterval(selectBackground, 300000); // 5 minutes
+
+// fill the news feed
--- src/make.js
+++ src/make.js
@@ -1,6 +1,25 @@
 const fs = require('fs');
 
 const Handlebars = require('handlebars');
+const jsYaml = require('js-yaml');
 const _ = require('lodash');
 
+const defaultData = {
+
+};
+
+let basData = fs.readFileSync('data.yml', 'utf-8');
+basData = jsYaml.safeLoad(basData);
+basData = { ...defaultData, ...basData };
+
+// compile sources
+function writeTemplate(src, dest, data) {
+    let sourceData = fs.readFileSync(src, 'utf-8');
+    let template = Handlebars.compile(sourceData);
+    let compiled = template(data);
+    fs.writeFile('../dist/'+dest, compiled, 'utf-8', err => {});
+}
+
+writeTemplate('www/index.html.h', 'index.html', basData);
+writeTemplate('www/script.js.h', 'script.js', basData);
 
